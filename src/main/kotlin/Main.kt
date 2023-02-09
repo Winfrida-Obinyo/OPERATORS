5a592c8e9f92d13ec9f72c98e95087744838a4e4
Winfrida Kemunto <obinyowinfridaa@gmail.com>
2023-02-09 07:40:51 +0300

    OPERATORS

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main(){
+    name()
+    add()
+    sum(67,60, 57, 40)
+    fact()
+}
+fun name (){
+    var name = "Winfrida"
+
+    println("Hello" + " " + name)
+
+}
+fun add(){
+    var y = 40
+    var z = 12
+    var sum = y % z
+    println(sum)
+
+}
+fun sum (num1:Int, num2:Int, num3:Int, num4:Int){
+    var sum = num1 + num2 + num3 + num4
+    println(sum)
+}
+fun fact(){
+    var fact = " I am an intelligent girl and i love coding"
+    println(fact)
+
+}
